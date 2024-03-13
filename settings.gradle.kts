8bd1f176683fda82ca1ed6970bc92611b3dcfe02
ActiVisioN-Tapok <novgkr@gmail.com>
2024-03-13 15:26:45 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CoffeeDesign2"
+include(":app")
+ 
